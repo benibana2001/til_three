b31817c7a6531946d3fd3d1b532f7207150dd39f
benibana2001 <benibana2001@gmail.com>
2022-01-29 13:41:58 +0900

    uniforms, attributes

--- src/script.js
+++ src/script.js
@@ -21,6 +21,7 @@ const scene = new THREE.Scene();
  * Textures
  */
 const textureLoader = new THREE.TextureLoader();
+const flagTeture = textureLoader.load('/textures/xxx.jpg');
 
 /**
  * Test mesh
@@ -42,10 +43,20 @@ geometry.setAttribute('aRandom', new THREE.BufferAttribute(randoms, 1));
 const material = new THREE.RawShaderMaterial({
   vertexShader: testVertexShader,
   fragmentShader: testFragmentShader,
+  uniforms: {
+    uFrequency: { value: new THREE.Vector2(10, 5) },
+    uTime: { value: 0 },
+    uColor: { value: new THREE.Color('orange') },
+    uTexture: { value: flagTeture },
+  },
 });
+
+gui.add(material.uniforms.uFrequency.value, 'x').min(0).max(20).step(0.01).name('frequencyX');
+gui.add(material.uniforms.uFrequency.value, 'y').min(0).max(20).step(0.01).name('frequencyY');
 
 // Mesh
 const mesh = new THREE.Mesh(geometry, material);
+// mesh.scale.y = 2/3
 scene.add(mesh);
 
 /**
@@ -103,6 +114,9 @@ const clock = new THREE.Clock();
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
+
+  // Update material
+  material.uniforms.uTime.value = elapsedTime;
 
   // Update controls
   controls.update();
