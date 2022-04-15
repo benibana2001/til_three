bc6205621bc7cdb79a6b60301b7cb539a7c9db4e
benibana2001 <benibana2001@gmail.com>
2022-04-15 23:14:29 +0900

    2022_04_14

--- src_2022_04_14/script.js
+++ src_2022_04_14/script.js
@@ -7,7 +7,10 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
  * Base
  */
 // Debug
-// const gui = new dat.GUI();
+const gui = new dat.GUI();
+const parameters = {
+    color: 0xff0000
+}
 
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
@@ -18,8 +21,44 @@ const scene = new THREE.Scene();
 // Object
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 
+// const geometry = new THREE.BufferGeometry();
+// const count = 2;
+// const positionsArray = new Float32Array(count * 3 * 3);
+// for(let i = 0; i < count * 3 * 3; i++) {
+//     positionsArray[i] = (Math.random() - 0.5) * 4
+// }
+
+// const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3);
+// geometry.setAttribute('position', positionsAttribute);
+
+// Texture
+const loadingManager = new THREE.LoadingManager();
+loadingManager.onStart = () => console.log('loading started')
+loadingManager.onLoad = () => console.log('loading finished')
+loadingManager.onProgress = () => console.log('loading progressing')
+loadingManager.onError = () => console.log('loading error')
+
+const textureLoader = new THREE.TextureLoader(loadingManager);
+
+const PATH_DOOR_TEXTURE = '/textures/door/'
+const colorTexture = textureLoader.load(`${PATH_DOOR_TEXTURE}color.jpg`);
+const colorTexture2 = textureLoader.load(`/textures/checkerboard-1024x1024.png`);//大きい画像
+const colorTexture3 = textureLoader.load('/textures/checkerboard-8x8.png');//小さい画像
+const colorTexture4 = textureLoader.load('/textures/minecraft.png');//小さい画像.maincraft
+const alphaTexture = textureLoader.load(`${PATH_DOOR_TEXTURE}alpha.jpg`);
+const heightTexture = textureLoader.load(`${PATH_DOOR_TEXTURE}height.jpg`);
+const normalTexture = textureLoader.load(`${PATH_DOOR_TEXTURE}normal.jpg`);
+const ambientOcclusionTexture = textureLoader.load(`${PATH_DOOR_TEXTURE}ambientOcclusion.jpg`);
+const metalnessTexture = textureLoader.load(`${PATH_DOOR_TEXTURE}metalness.jpg`);
+const roughnessTexture = textureLoader.load(`${PATH_DOOR_TEXTURE}roughness.jpg`);
+
+colorTexture.minFilter = THREE.NearestFilter;
+colorTexture.generateMipmaps = false
+
+colorTexture3.magFilter = THREE.NearestFilter;
+colorTexture4.magFilter = THREE.NearestFilter;
 // Material
-const material = new THREE.MeshBasicMaterial({color: 0xff0000});
+const material = new THREE.MeshBasicMaterial({map: colorTexture4});
 
 // Mesh
 const mesh = new THREE.Mesh(geometry, material);
@@ -69,7 +108,7 @@ window.addEventListener('dblclick', () => {
 
 // Camera
 const fov = 75;
-const camera = new THREE.PerspectiveCamera(fov, sizes.width / sizes.height, 1, 1000)
+const camera = new THREE.PerspectiveCamera(fov, sizes.width / sizes.height, 0.1, 1000)
 
 camera.position.z = 3;
 camera.lookAt(mesh.position)
@@ -101,3 +140,14 @@ const tick = () => {
     window.requestAnimationFrame(tick);
 }
 tick()
+
+/**
+ * Debug
+ */
+// gui.add(mesh.position, 'y', - 3, 3, 0.01);
+gui.add(mesh.position, 'y').min(-3).max(3).step(0.01).name("高さ");
+gui.add(mesh, 'visible')
+gui.add(material, 'wireframe')
+gui.addColor(parameters, 'color').onChange(() => {
+    material.color.set(parameters.color)
+})
